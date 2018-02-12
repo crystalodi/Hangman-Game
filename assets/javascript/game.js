aeac8d8826f7343585c9596f9ad1a2e581c4bc34
Crystal O <crystalodi@gmail.com>
2018-02-11 22:36:32 -0600

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+//Define dictionary array
+//Define guessed letters array
+//Define selectedWord variable
+//Define number of guesses variable and set it to 15
+
+//If user presses any key THEN
+
+    //Game Starts
+
+        //Random word chosen from dictionary array
+
+        //Generate html for the number of blank spaces that words will fill
+
+            //IF User presses a letter key that hasn't been guessed
+
+                //If key pressed is one of the letters in the selected word
+                    //Add letter to guessed letters array and display in section for guessed letters
+                    //Populate letter in correct spot(s) on game space where word is supposed to be
+                    //IF all letters have been correctly guessed
+                        //increment win counter
+                        //start new game
+                        
+
+                //If key pressed is letter that is not in random word THEN
+                    //Decrement number of guesses variable by 1
+                    //Add letter to guessed letters array
+                    //If user has run out of guesses
+                        //increment loss counter
+                        //start new game
+                    
+            //ELSE 
+                //do nothing (probably won't go in code)
